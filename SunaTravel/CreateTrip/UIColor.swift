14207ba7a90486ace604ac9385eeb2348d36e827
Lilia-Chechina <146482951+Lilia-Chechina@users.noreply.github.com>
2025-02-20 01:19:54 +0300

    Update UIColor.swift
    .

--- SunaTravel/CreateTrip/UIColor.swift
+++ SunaTravel/CreateTrip/UIColor.swift
@@ -1,14 +1,5 @@
 import UIKit
 
-//extension UIColor {
-//    convenience init(hex: String) {
-//        let hexNumber = Int(hex, radix: 16) ?? 0
-//        self.init(red: CGFloat((hexNumber >> 16) & 0xFF) / 255.0,
-//                  green: CGFloat((hexNumber >> 8) & 0xFF) / 255.0,
-//                  blue: CGFloat(hexNumber & 0xFF) / 255.0,
-//                  alpha: 1.0)
-//    }
-//}
 extension UIColor {
     convenience init(hex: String, alpha: CGFloat = 1.0) {
         var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
