59cf14e7401f5c2718b8b5860b1422795257f452
vanechka <77458309+IvanTarasiuk@users.noreply.github.com>
2025-05-11 20:42:02 +0300

    Revert "Revert "Revert "App/feature/profile data"""

--- SunaTravel/Profile/ViewModels/ProfileViewModel.swift
+++ SunaTravel/Profile/ViewModels/ProfileViewModel.swift
@@ -1,56 +1,26 @@
-import Firebase
-import FirebaseFirestore
-import FirebaseFirestoreSwift
+//
+//  ProfileViewModel.swift
+//  SunaTravel
+//
+//  Created by Иван Тарасюк on 24.12.2024.
+//
+
+import SwiftUI
 
 class ProfileViewModel: ObservableObject {
     @Published var profile: ProfileData
-    
-    private let db = Firestore.firestore()
+    @Published var isSuccessMessageShowed: Bool = false
     
     init(profile: ProfileData) {
         self.profile = profile
     }
-
-    func loadProfileData(for uid: String) {
-        db.collection("userData").document(uid).getDocument { snapshot, error in
-            if let data = snapshot?.data(), error == nil {
-                DispatchQueue.main.async {
-                    self.profile.name = data["name"] as? String ?? ""
-                    self.profile.email = data["email"] as? String ?? ""
-                    self.profile.phoneNumber = data["phoneNumber"] as? String ?? ""
-                    self.profile.location = data["location"] as? String ?? ""
-                    self.profile.rewardPointsCount = data["rewardPointsCount"] as? Int ?? 0
-                    self.profile.travelTipsCount = data["travelTipsCount"] as? Int ?? 0
-                    self.profile.bucketListCount = data["bucketListCount"] as? Int ?? 0
-                }
-            } else {
-                print("Ошибка загрузки профиля: \(error?.localizedDescription ?? "Unknown error")")
-            }
-        }
-    }
-
+    
     func saveChanges(name: String, email: String, location: String, phoneNumber: String, avatar: UIImage?) {
-        guard let uid = Auth.auth().currentUser?.uid else { return }
-
-        let userData: [String: Any] = [
-            "name": name,
-            "email": email,
-            "location": location,
-            "phoneNumber": phoneNumber
-        ]
-
-        db.collection("userData").document(uid).setData(userData) { error in
-            if let error = error {
-                print("Ошибка сохранения: \(error.localizedDescription)")
-            } else {
-                DispatchQueue.main.async {
-                    self.profile.name = name
-                    self.profile.email = email
-                    self.profile.location = location
-                    self.profile.phoneNumber = phoneNumber
-                    self.profile.avatar = avatar
-                }
-            }
-        }
+        profile.name = name
+        profile.email = email
+        profile.location = location
+        profile.phoneNumber = phoneNumber
+        profile.avatar = avatar
+        isSuccessMessageShowed = true
     }
 }
