cd9dfc82f8d910e9df8406be04b8d23f811c43be
Mikhail <zimanov.mm@mail.ru>
2025-05-12 02:41:56 +0300

    Make profile data load

--- SunaTravel/Profile/ViewModels/ProfileViewModel.swift
+++ SunaTravel/Profile/ViewModels/ProfileViewModel.swift
@@ -1,26 +1,57 @@
-//
-//  ProfileViewModel.swift
-//  SunaTravel
-//
-//  Created by Иван Тарасюк on 24.12.2024.
-//
-
-import SwiftUI
+import Firebase
+import FirebaseFirestore
+import FirebaseFirestoreSwift
 
 class ProfileViewModel: ObservableObject {
     @Published var profile: ProfileData
-    @Published var isSuccessMessageShowed: Bool = false
+    
+    private let db = Firestore.firestore()
     
     init(profile: ProfileData) {
         self.profile = profile
     }
-    
+
+    func loadProfileData(for uid: String) {
+        db.collection("userData").document(uid).getDocument { snapshot, error in
+            if let data = snapshot?.data(), error == nil {
+                DispatchQueue.main.async {
+                    self.profile.name = data["name"] as? String ?? "hg"
+                    self.profile.email = data["email"] as? String ?? ""
+                    self.profile.phoneNumber = data["phoneNumber"] as? String ?? ""
+                    self.profile.location = data["location"] as? String ?? ""
+                    self.profile.rewardPointsCount = data["rewardPointsCount"] as? Int ?? 0
+                    self.profile.travelTipsCount = data["travelTipsCount"] as? Int ?? 0
+                    self.profile.bucketListCount = data["bucketListCount"] as? Int ?? 0
+                }
+            } else {
+                print("Ошибка загрузки профиля: \(error?.localizedDescription ?? "Unknown error")")
+            }
+        }
+    }
+
     func saveChanges(name: String, email: String, location: String, phoneNumber: String, avatar: UIImage?) {
-        profile.name = name
-        profile.email = email
-        profile.location = location
-        profile.phoneNumber = phoneNumber
-        profile.avatar = avatar
-        isSuccessMessageShowed = true
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        print("gg")
+        print(uid)
+        let userData: [String: Any] = [
+            "name": name,
+            "email": email,
+            "location": location,
+            "phoneNumber": phoneNumber
+        ]
+
+        db.collection("userData").document(uid).setData(userData) { error in
+            if let error = error {
+                print("Ошибка сохранения: \(error.localizedDescription)")
+            } else {
+                DispatchQueue.main.async {
+                    self.profile.name = name
+                    self.profile.email = email
+                    self.profile.location = location
+                    self.profile.phoneNumber = phoneNumber
+                    self.profile.avatar = avatar
+                }
+            }
+        }
     }
 }
